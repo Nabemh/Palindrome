ff589b6210595355405fd2e6322d975268f27285
Nabemh <nhanior@gmail.com>
2024-05-28 14:58:38 +0100

    palindrome execise

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package Palindrome;
 public class Main {
     public static void main(String[] args) {
         PalindromeCheck palindromeChecker = new PalindromeCheck();
-        String str = "Tunde Ednut";
+        String str = "civic";
         if (palindromeChecker.isPalindrome(str)) {
             System.out.println("True");
         } else {
